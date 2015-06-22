8a00ab223f53570cce1044f328fa324da2f6d395
tkmagesh <tkmagesh77@gmail.com>
2015-06-22 14:02:12 +0530

    Calculator - Completed

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -1,3 +1,20 @@
 function add(x,y){
-    return x + y;
+    function parseArg(n){
+        if (Array.isArray(n)) return add.apply(this, n);
+        if (typeof n === "function") return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n,10);
+    }
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
+
+/*
+When a function is invoked
+1. As a method of an obj
+    this -> obj
+2. As a function
+    this -> global scope (window)
+3. Using the call method
+4. Using the apply method
+
+
+*/
