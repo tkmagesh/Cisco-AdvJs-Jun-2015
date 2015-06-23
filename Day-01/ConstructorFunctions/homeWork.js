e02151615c893d6123e8e144ebfd8717120a7d17
tkmagesh <tkmagesh77@gmail.com>
2015-06-23 10:44:38 +0530

    FP - Sort - Seed

--- Day-01/ConstructorFunctions/homeWork.js
+++ Day-01/ConstructorFunctions/homeWork.js
@@ -1,4 +1,4 @@
-Create a "class" by name "SalaryCalculator".
+/*Create a "class" by name "SalaryCalculator".
 The SalaryCalculator should have the following members
     basic
     hra
@@ -8,5 +8,43 @@ The SalaryCalculator should have the following members
     calculate()
 
 The user should be able to initialize the SalaryCalculator with basic, hra, da, and tax.
-The salary should be calculated when the user invokes the "calculate()" method and assigned to 'salary'
+The salary should be calculated when the user invokes the "calculate()" method and assigned to 'salary'*/
+
+function SalaryCalculator(basic, hra, da, tax){
+    this.basic = basic;
+    this.hra = hra;
+    this.da = da;
+    this.tax = tax;
+    this.salary = 0;
+
+    this.calculate = function(){
+        var gross = this.basic + this.hra + this.da;
+        this.salary = gross * ((100-this.tax)/100);
+    }
+}
+
+
+/*Make salary readonly*/
+
+function SalaryCalculator(basic, hra, da, tax){
+    this.basic = basic;
+    this.hra = hra;
+    this.da = da;
+    this.tax = tax;
+    this.__salary = 0;
+}
+SalaryCalculator.prototype.getSalary = function(){
+        return this.__salary;
+    };
+SalaryCalculator.prototype.calculate = function(){
+    var gross = this.basic + this.hra + this.da;
+    this.__salary = gross * ((100-this.tax)/100);
+}
+
+
+
+
+
+
+
 
---
+++ Day-02/02-FunctionalProgramming/products.js
@@ -0,0 +1,81 @@
+var products = [
+    {id : 5, name : "Pen", cost : 50, units : 60, category : 2},
+    {id : 9, name : "Hen", cost : 90, units : 40, category : 1},
+    {id : 7, name : "Den", cost : 40, units : 70, category : 2},
+    {id : 3, name : "Zen", cost : 60, units : 80, category : 1},
+    {id : 8, name : "Ken", cost : 70, units : 50, category : 2},
+    {id : 2, name : "Ten", cost : 20, units : 30, category : 1},
+]
+
+function describe(title, fn){
+    console.group(title);
+    fn();
+    console.groupEnd();
+}
+
+
+describe("Initial List", function(){
+    console.table(products);
+});
+describe("Sort", function(){
+    describe("Default Sorting [by id]", function(){
+        function Sort(){
+            for(var i=0; i<products.length-1; i++)
+                for(var j=i + 1; j<products.length; j++){
+                    var left = products[i],
+                        right = products[j];
+                    if (left.id > right.id){
+                        products[i] = products[j];
+                        products[j] = left;
+                    }
+                }
+        }
+        Sort();
+        console.table(products);
+    });
+    describe("Sorting by attributes", function(){
+        function Sort(attrName){
+            for(var i=0; i<products.length-1; i++)
+                for(var j=i + 1; j<products.length; j++){
+                    var left = products[i],
+                        right = products[j];
+                    if (left[attrName] > right[attrName]){
+                        products[i] = products[j];
+                        products[j] = left;
+                    }
+                }
+        }
+        describe("by cost", function(){
+            Sort("cost");
+            console.table(products);
+        });
+        describe("by units", function(){
+            Sort("units");
+            console.table(products);
+        });
+    });
+    describe("Sorting by any logic", function(){
+        function Sort(){
+            //fill in the blanks
+
+        }
+        describe("by value [cost * units]", function(){
+
+        })
+    })
+});
+
+
+/*
+Sort
+Filter
+ForEach
+Min
+Max
+CountBy
+Avg
+Aggregate
+All
+Any
+GroupBy
+*/
